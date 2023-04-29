c8da3b25635647fd9cd8cd1834d6645ab66a9d42
Muzaffar55 <97522350+Muzaffar55@users.noreply.github.com>
2023-04-29 14:36:18 +0500

    Update BasicSwitch.java
    Update this file

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -4,9 +4,6 @@ public class BasicSwitch {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
-            default:
-                System.out.println("default");
-                break;
             case 7:
                 System.out.println("seven");
                 break;
@@ -16,6 +13,8 @@ public class BasicSwitch {
             case 20:
                 System.out.println("twenty");
                 break;
+            default:
+                System.out.println("default");
         }
     }
 }
